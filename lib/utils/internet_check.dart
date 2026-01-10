9ea426044ddc452e3699d3c3e11d1d82a44163ba
wajhi <wajhi.bzns2024@gmail.com>
2026-01-10 15:21:39 +0500

    Add debug print statement for internet connectivity check

--- lib/utils/internet_check.dart
+++ lib/utils/internet_check.dart
@@ -2,9 +2,11 @@ import 'dart:io';
 
 Future<bool> hasInternet() async {
   try {
+    print('Checking internet connectivity...');
     final result = await InternetAddress.lookup('google.com');
     return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
   } catch (_) {
     return false;
+
   }
 }
