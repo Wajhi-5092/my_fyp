5ddf67488b9f66ea64ca3c9b408d24aed9e3b99d
wajhi <wajhi.bzns2024@gmail.com>
2026-01-10 15:25:20 +0500

    Remove debug print statement from internet connectivity check

--- lib/utils/internet_check.dart
+++ lib/utils/internet_check.dart
@@ -2,11 +2,10 @@ import 'dart:io';
 
 Future<bool> hasInternet() async {
   try {
-    print('Checking internet connectivity...');
     final result = await InternetAddress.lookup('google.com');
     return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
   } catch (_) {
     return false;
-
+    
   }
 }
